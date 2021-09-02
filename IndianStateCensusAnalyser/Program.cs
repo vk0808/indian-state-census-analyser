6295e7489a63b471d9d9d4423f386d34b110d964
VINOD KUMAR R <ravivinod21@gmail.com>
2021-09-02 13:11:44 +0530

    feat: initial commit

---
+++ IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indian State Census Analyser");
+        }
+    }
+}
